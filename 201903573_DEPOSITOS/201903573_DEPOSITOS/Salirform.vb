6bd98344a28faf628305315da7a2f82e253e76eb
lidiaorozco246 <88960193+lidiaorozco246@users.noreply.github.com>
2021-08-15 21:56:25 -0600

    Tarea3_201903573

---
+++ 201903573_DEPOSITOS/201903573_DEPOSITOS/Salirform.vb
@@ -0,0 +1,15 @@
+﻿Public Class Salirform
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles BTNsi.Click
+        End
+    End Sub
+
+    Private Sub BTNno_Click(sender As Object, e As EventArgs) Handles BTNno.Click
+        Dim formPrincipal As Form1 = New Form1
+        Me.Hide()
+        formPrincipal.Show()
+    End Sub
+End Class
